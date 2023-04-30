389572db85948f600be3bb211df9f451c44cffa6
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-04-30 18:01:16 -0400

    did my best

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -25,140 +25,29 @@ function fetchAPI(city) {
   }).then(function (data) {
     console.log(data);
     cityName.innerHTML = data.name;
-    temp.innerHTML = "Temp: " + Math.floor(data.main.temp) + "&#176F";
-    wind.innerHTML = "Wind: " + Math.floor(data.wind.speed) + "mph";
-    hum.innerHTML = "Humidity: " + Math.floor(data.main.humidity) + "%";
+    temp.innerHTML = "Temp: " + Math.floor(data.main.temp) + "&#176F" + " Wind: " + Math.floor(data.wind.speed) + "mph" + " Humidity: " + Math.floor(data.main.humidity) + "%"; // wind.innerHTML = "Wind: " + Math.floor(data.wind.speed) + "mph"
+    // hum.innerHTML = "Humidity: " + Math.floor(data.main.humidity) + "%"
   });
-  btnSearch.addEventListener("click", function () {
-    var city = citiesSearch.value;
-    fetchAPI(city);
+  var secondDay = document.getElementById("secondDay");
+  var secondDayTemp = document.getElementById("tempNextDay");
+  var secondDayWind = document.getElementById("windNextDay");
+  var secondDayHum = document.getElementById("humNextDay");
+  var futureWeather = "https://api.openweathermap.org/data/2.5/forecast?q=".concat(city, "&appid=e52a29666bfe7798641471ef3c524f6e&units=imperial");
+  fetch(futureWeather).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    console.log(data);
+    secondDay.innerHTML = data.list[2].dt_txt;
+    secondDayTemp.innerHTML = " Temp: " + Math.floor(data.list[2].main.temp) + "&#176F";
+    secondDayWind = " Wind: " + Math.floor(data.list[2].wind.speed) + "mph";
+    secondDayHum = " Humidity: " + Math.floor(data.list[2].main.humidity) + "%";
   });
+}
 
-  function fetchAPI(city) {
-    (function (res) {
-      return res.json();
-    }).then(function (data) {
-      console.log(data);
-      fiveDayWeather.innerHTML = ""; // clear previous content
-
-      for (var i = 0; i < data.list.length; i += 8) {
-        // get data for every 24 hours (8 * 3 hours)
-        var forecastData = data.list[i];
-        var forecastDiv = document.createElement("div");
-        forecastDiv.innerHTML = "\n          <h3>".concat(forecastData.dt_txt, "</h3>\n          <p>Temp: ").concat(Math.floor(forecastData.main.temp), "&#176F</p>\n          <p>Wind: ").concat(Math.floor(forecastData.wind.speed), "mph</p>\n          <p>Humidity: ").concat(Math.floor(forecastData.main.humidity), "%</p>\n        ");
-        fiveDayWeather.appendChild(forecastDiv);
-      }
-
-      cityName.innerHTML = data.city.name;
-      temp.innerHTML = "Temp: " + Math.floor(data.list[0].main.temp) + "&#176F";
-      wind.innerHTML = "Wind: " + Math.floor(data.list[0].wind.speed) + "mph";
-      hum.innerHTML = "Humidity: " + Math.floor(data.list[0].main.humidity) + "%";
-    }); // extract the data for the second card
-    // replace "forecastDiv" with the ID of your second card's container element
-
-    var forecastDiv2 = document.getElementById("forecastDiv2");
-
-    for (var i = 8; i <= 15; i++) {
-      var forecastData = data.list[i];
-      var date = new Date(forecastData.dt_txt);
-      var day = daysOfWeek[date.getDay()];
-      var iconUrl = "https://openweathermap.org/img/w/".concat(forecastData.weather[0].icon, ".png");
-      var forecastHTML = "\n          <div class=\"forecast-day\">\n            <div>".concat(day, "</div>\n            <img src=\"").concat(iconUrl, "\" alt=\"").concat(forecastData.weather[0].description, "\">\n            <div>").concat(Math.floor(forecastData.main.temp), "&#176F</div>\n          </div>\n        ");
-      forecastDiv2.insertAdjacentHTML("beforeend", forecastHTML);
-    }
-
-    var forecastDiv3 = document.getElementById("forecastDiv3");
-    forecastDiv3.innerHTML = "dayThree";
-
-    for (var _i = 16; _i <= 23; _i++) {
-      var _forecastData = data.list[_i];
-
-      var _date = new Date(_forecastData.dt_txt);
-
-      var _day = daysOfWeek[_date.getDay()];
-
-      var _iconUrl = "https://openweathermap.org/img/w/".concat(_forecastData.weather[0].icon, ".png");
-
-      var _forecastHTML = "\n          <div class=\"forecast-day\">\n            <div>".concat(_day, "</div>\n            <img src=\"").concat(_iconUrl, "\" alt=\"").concat(_forecastData.weather[0].description, "\">\n            <div>").concat(Math.floor(_forecastData.main.temp), "&#176F</div>\n          </div>\n        ");
-
-      forecastDiv3.insertAdjacentHTML("beforeend", _forecastHTML);
-    }
-
-    var forecastDiv4 = document.getElementById("forecastDiv4");
-    forecastDiv4.innerHTML = "";
-
-    for (var _i2 = 24; _i2 <= 31; _i2++) {
-      var _forecastData2 = data.list[_i2];
-
-      var _date2 = new Date(_forecastData2.dt_txt);
-
-      var _day2 = daysOfWeek[_date2.getDay()];
-
-      var _iconUrl2 = "https://openweathermap.org/img/w/".concat(_forecastData2.weather[0].icon, ".png");
-
-      var _forecastHTML2 = "\n          <div class=\"forecast-day\">\n            <div>".concat(_day2, "</div>\n            <img src=\"").concat(_iconUrl2, "\" alt=\"").concat(_forecastData2.weather[0].description, "\">\n            <div>").concat(Math.floor(_forecastData2.main.temp), "&#176F</div>\n            </div>\n          ");
-
-      forecastDiv3.insertAdjacentHTML("beforeend", _forecastHTML2);
-    }
-
-    btnSearch.addEventListener("click", function () {
-      var city = citiesSearch.value;
-      fetchAPI(city);
-      fetchFiveDayForecast(city);
-    });
-  } // extract the data for the first card
-
-} // ://api..org/data/2.5/weather?q=${city}&appid=<YOUR_APP_ID>&units=imperial`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       // Update the HTML for the main weather card
-//       const cityName = document.getElementById("cityName");
-//       const temp = document.getElementById("temp");
-//       const wind = document.getElementById("wind");
-//       const hum = document.getElementById("hum");
-//       cityName.innerHTML = data.name;
-//       temp.innerHTML = `Temperature: ${Math.round(data.main.temp)} &#176;F`;
-//       wind.innerHTML = `Wind: ${Math.round(data.wind.speed)} mph`;
-//       hum.innerHTML = `Humidity: ${data.main.humidity}%`;
-//       // Fetch and display the five-day forecast
-//       fetchFiveDayForecast(city);
-//     })
-//     .catch((error) => console.log(error));
-// }
-
-/* <h5 class="card-title">${new Date(item.dt * 1000).toLocaleDateString()}</h5>
-            <p class="card-text">Temperature: ${Math.round(item.main.temp)} &#176;F</p>
-            <p class="card-text">Humidity: ${item.main.humidity}%</p>
-            <p class="card-text">Wind: ${item.wind.speed} mph</p> */
-// let cityName = document.getElementById('inputCity');
-// let searchBtn = document.getElementById('btnSearch');
-// console.log('inputCity');
-// searchBtn.addEventListener('click', function (event) {
-//   event.preventDefault()
-//   if (event.target.id === 'btnSearch') {
-//     cityName = document.getElementById("inputCity").value;
-//     const cityForeCast = "api..org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e52a29666bfe7798641471ef3c524f6e"
-//   fetch(cityForeCast).then(function (response){
-//     return response.json()
-//   }).then(function(data) {
-//     console.log('list')
-//   })
-//   }
-// })
-// document.getElementById("searchBtn").addEventListener("click", function () {
-//   var city = document.getElementById("cities").value;
-//   var apiKey = "{e52a29666bfe7798641471ef3c524f6e}";
-//   var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e52a29666bfe7798641471ef3c524f6e" + city + "&appid=" + apiKey;
-//   $.ajax({
-//     url: apiUrl,
-//     method: "GET",
-//     dataType: "json",
-//     success: function (response) {
-//       document.append()
-//     }
-//   });
+btnSearch.addEventListener("click", function () {
+  var city = citiesSearch.value;
+  fetchAPI(city);
+}); // btnSearch.addEventListener("click", function () {
+//   const city = citiesSearch.value;
+//   fetchAPI(city)
 // });
-// var temp = response.main.temp;
-// var desc = response.weather[0].description;
-// document.querySelector("#chosenCity .card-title").textContent = city;
-// document.querySelector("#chosenCity .card-text").textContent = "Temperature: " + temp + "°F, " + desc;
--- script.js
+++ script.js
@@ -11,15 +11,16 @@
 //  "New York"
 //];
 
-let citiesSearch = document.getElementById("citiesSearch")
+const citiesSearch = document.getElementById("citiesSearch")
 const btnSearch = document.getElementById("btnSearch")
 const cityName = document.getElementById("cityName")
 const temp = document.getElementById("temp")
 const wind = document.getElementById("wind")
 const hum = document.getElementById("hum")
+const weatherIcon = document.getElementById("weatherIcon")
 
 function fetchAPI(city) {
-
+// this first fetch is for the main card
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e52a29666bfe7798641471ef3c524f6e&units=imperial`)
     .then((res) => {
       return res.json()
@@ -30,7 +31,104 @@ function fetchAPI(city) {
       temp.innerHTML = "Temp: " + Math.floor(data.main.temp) + "&#176F"
       wind.innerHTML = "Wind: " + Math.floor(data.wind.speed) + "mph"
       hum.innerHTML = "Humidity: " + Math.floor(data.main.humidity) + "%"
+      weatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
     })
+
+    // This is the first card below the main card that will display the second day Weather Forecast
+  const secondDay = document.getElementById("secondDay")
+  const secondDayTemp = document.getElementById("secondDayTemp")
+  const secondDayWind = document.getElementById("secondDayWind")
+  const secondDayHum = document.getElementById("secondDayHum")
+  let futureWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e52a29666bfe7798641471ef3c524f6e&units=imperial`
+
+  fetch(futureWeather)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data1) => {
+      console.log(data1)
+      secondDay.innerHTML = data1.list[0].dt_txt;
+      secondDayTemp.innerHTML = "Temp: " + Math.floor(data1.list[0].main.temp) + "&#176F";
+      secondDayWind.innerHTML = "Wind: " + Math.floor(data1.list[0].wind.speed) + "mph";
+      secondDayHum.innerHTML = "Humidity: " + Math.floor(data1.list[0].main.humidity) + "%";
+      secondDayWeatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data1.list[0].weather[0].icon}.png`);
+    });
+
+    // This is the second card below the main card that will display the third day Weather Forecast
+    const thirdDay = document.getElementById("thirdDay")
+    const thirdDayTemp = document.getElementById("thirdDayTemp")
+  const thirdDayWind = document.getElementById("thirdDayWind")
+  const thirdDayHum = document.getElementById("thirdDayHum")
+
+  fetch(futureWeather)
+  .then((res) => {
+    return res.json()
+  })
+  .then((data2) => {
+    console.log(data2)
+    thirdDay.innerHTML = data2.list[8].dt_txt;
+    thirdDayTemp.innerHTML = "Temp: " + Math.floor(data2.list[8].main.temp) + "&#176F";
+    thirdDayWind.innerHTML = "Wind: " + Math.floor(data2.list[8].wind.speed) + "mph";
+    thirdDayHum.innerHTML = "Humidity: " + Math.floor(data2.list[8].main.humidity) + "%";
+    thirdDayWeatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data2.list[8].weather[0].icon}.png`);
+  });
+  
+  // This is the third card below the main card that will display the fourth days Weather Forecast
+  const fourthDay = document.getElementById("fourthDay")
+    const fourthDayTemp = document.getElementById("fourthDayTemp")
+  const fourthDayWind = document.getElementById("fourthDayWind")
+  const fourthDayHum = document.getElementById("fourthDayHum")
+
+  fetch(futureWeather)
+  .then((res) => {
+    return res.json()
+  })
+  .then((data3) => {
+    console.log(data3)
+    fourthDay.innerHTML = data3.list[16].dt_txt;
+    fourthDayTemp.innerHTML = "Temp: " + Math.floor(data3.list[16].main.temp) + "&#176F";
+    fourthDayWind.innerHTML = "Wind: " + Math.floor(data3.list[16].wind.speed) + "mph";
+    fourthDayHum.innerHTML = "Humidity: " + Math.floor(data3.list[16].main.humidity) + "%";
+    fourthDayWeatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data3.list[16].weather[0].icon}.png`);
+  });
+
+  // This is the fourth card below the main card that will display the fifth day Weather Forecast
+  const fifthDay = document.getElementById("fifthDay")
+    const fifthDayTemp = document.getElementById("fifthDayTemp")
+  const fifthDayWind = document.getElementById("fifthDayWind")
+  const fifthDayHum = document.getElementById("fifthDayHum")
+
+  fetch(futureWeather)
+  .then((res) => {
+    return res.json()
+  })
+  .then((data4) => {
+    console.log(data4)
+    fifthDay.innerHTML = data4.list[24].dt_txt;
+    fifthDayTemp.innerHTML = "Temp: " + Math.floor(data4.list[24].main.temp) + "&#176F";
+    fifthDayWind.innerHTML = "Wind: " + Math.floor(data4.list[24].wind.speed) + "mph";
+    fifthDayHum.innerHTML = "Humidity: " + Math.floor(data4.list[24].main.humidity) + "%";
+    fifthDayWeatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data4.list[24].weather[0].icon}.png`);
+  });
+
+  // This is the fifth card below the main card that will display the sixth day Weather Forecast
+  const sixthDay = document.getElementById("sixthDay")
+    const sixthDayTemp = document.getElementById("sixthDayTemp")
+  const sixthDayWind = document.getElementById("sixthDayWind")
+  const sixthDayHum = document.getElementById("sixthDayHum")
+
+  fetch(futureWeather)
+  .then((res) => {
+    return res.json()
+  })
+  .then((data5) => {
+    console.log(data5)
+    sixthDay.innerHTML = data5.list[32].dt_txt;
+    sixthDayTemp.innerHTML = "Temp: " + Math.floor(data5.list[32].main.temp) + "&#176F";
+    sixthDayWind.innerHTML = "Wind: " + Math.floor(data5.list[32].wind.speed) + "mph";
+    sixthDayHum.innerHTML = "Humidity: " + Math.floor(data5.list[32].main.humidity) + "%";
+    sixthDayWeatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${data5.list[32].weather[0].icon}.png`);
+  });
 }
 
 btnSearch.addEventListener("click", function () {
@@ -39,134 +137,29 @@ btnSearch.addEventListener("click", function () {
 });
 
 
-function fetchAPI(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e52a29666bfe7798641471ef3c524f6e&units=imperial`)
-    .then((res) => {
-      return res.json()
-    })
-    .then((data) => {
-      console.log(data);
-      fiveDayWeather.innerHTML = ""; // clear previous content
-      for (let i = 0; i < data.list.length; i += 8) { // get data for every 24 hours (8 * 3 hours)
-        let forecastData = data.list[i];
-        let forecastDiv = document.createElement("div");
-        forecastDiv.innerHTML = `
-          <h3>${forecastData.dt_txt}</h3>
-          <p>Temp: ${Math.floor(forecastData.main.temp)}&#176F</p>
-          <p>Wind: ${Math.floor(forecastData.wind.speed)}mph</p>
-          <p>Humidity: ${Math.floor(forecastData.main.humidity)}%</p>
-        `;
-        fiveDayWeather.appendChild(forecastDiv);
-      }
-      cityName.innerHTML = data.city.name;
-      temp.innerHTML = "Temp: " + Math.floor(data.list[0].main.temp) + "&#176F";
-      wind.innerHTML = "Wind: " + Math.floor(data.list[0].wind.speed) + "mph";
-      hum.innerHTML = "Humidity: " + Math.floor(data.list[0].main.humidity) + "%";
-    })
-    const forecastDiv2 = document.getElementById("forecastDiv2");
-    
-    for (let i = 8; i <= 15; i++) {
-      let forecastData = data.list[i]; // THIS IS WHERE THE ERROR IS IN THE CONSOLE
-      const date = new Date(forecastData.dt_txt);
-      const day = daysOfWeek[date.getDay()];
-      const iconUrl = `https://openweathermap.org/img/w/${forecastData.weather[0].icon}.png`;
-      const forecastHTML = `
-        <div class="forecast-day">
-          <div>${day}</div>
-          <img src="${iconUrl}" alt="${forecastData.weather[0].description}">
-          <div>${Math.floor(forecastData.main.temp)}&#176F</div>
-        </div>
-      `;
-      forecastDiv2.insertAdjacentHTML("beforeend", forecastHTML);
-    }
-}
-
-// extract the data for the first card
 
 
 
+// Below is where I was trying to get the city buttons on the side of the page to work
 
-
-// ://api.openweathermap.org/data/2.5/weather?q=${city}&appid=<YOUR_APP_ID>&units=imperial`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       // Update the HTML for the main weather card
-//       const cityName = document.getElementById("cityName");
-//       const temp = document.getElementById("temp");
-//       const wind = document.getElementById("wind");
-//       const hum = document.getElementById("hum");
-
-//       cityName.innerHTML = data.name;
-//       temp.innerHTML = `Temperature: ${Math.round(data.main.temp)} &#176;F`;
-//       wind.innerHTML = `Wind: ${Math.round(data.wind.speed)} mph`;
-//       hum.innerHTML = `Humidity: ${data.main.humidity}%`;
-
-//       // Fetch and display the five-day forecast
-//       fetchFiveDayForecast(city);
-//     })
-//     .catch((error) => console.log(error));
-// }
-
-
-
-
-/* <h5 class="card-title">${new Date(item.dt * 1000).toLocaleDateString()}</h5>
-            <p class="card-text">Temperature: ${Math.round(item.main.temp)} &#176;F</p>
-            <p class="card-text">Humidity: ${item.main.humidity}%</p>
-            <p class="card-text">Wind: ${item.wind.speed} mph</p> */
-
-
-
-
-
-
-// let cityName = document.getElementById('inputCity');
-// let searchBtn = document.getElementById('btnSearch');
-
-// console.log('inputCity');
-
-// searchBtn.addEventListener('click', function (event) {
-//   event.preventDefault()
-//   if (event.target.id === 'btnSearch') {
-//     cityName = document.getElementById("inputCity").value;
-//     const cityForeCast = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e52a29666bfe7798641471ef3c524f6e"
-
-//   fetch(cityForeCast).then(function (response){
-//     return response.json()
-//   }).then(function(data) {
-//     console.log('list')
-//   })
-//   }
+// const atlantaBtn = document.getElementById("atlantaBtn")
+// const atlantaTemp = document.getElementsByClassName("list-group-item")
+// const atlantaWind = document.getElementsByClassName("list-group-item")
+// const atlantaHum = document.getElementsByClassName("list-group-item")
+// fetch(`https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=e52a29666bfe7798641471ef3c524f6e&units=imperial`)
+// .then((res) => {
+//   return res.json()
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("searchBtn").addEventListener("click", function () {
-//   var city = document.getElementById("cities").value;
-//   var apiKey = "{e52a29666bfe7798641471ef3c524f6e}";
-//   var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e52a29666bfe7798641471ef3c524f6e" + city + "&appid=" + apiKey;
-
-//   $.ajax({
-//     url: apiUrl,
-//     method: "GET",
-//     dataType: "json",
-//     success: function (response) {
-//       document.append()
-//     }
-//   });
+// .then((data6) => {
+//   console.log(data6)
+//   cityName.innerHTML = data6.name;
+//   atlantaTemp.innerHTML = "Temp: " + Math.floor(data6.main.temp) + "&#176F"
+//   atlantaWind.innerHTML = "Wind: " + Math.floor(data6.wind.speed) + "mph"
+//   atlantaHum.innerHTML = "Humidity: " + Math.floor(data6.main.humidity) + "%"
+// })
+// // fetch(Atlanta)
+// // console.log(Atlanta)
+// atlantaBtn.addEventListener("click", function () {
+//   const atlantaBtn = citiesSearch.value;
+//   fetchAPI(atlantaBtn)
 // });
-// var temp = response.main.temp;
-// var desc = response.weather[0].description;
-
-// document.querySelector("#chosenCity .card-title").textContent = city;
-// document.querySelector("#chosenCity .card-text").textContent = "Temperature: " + temp + "°F, " + desc;
